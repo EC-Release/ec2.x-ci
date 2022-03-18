1280409245580088e74d7eaef77a5fa088234630
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2022-03-17 19:55:31 -0700

    Update seeder.sh

--- seeder.sh
+++ seeder.sh
@@ -46,7 +46,7 @@ do
       #echo - connecting log host: "$EC_SEED_HOST"
       sk=$(getSdcTkn "$EC_API_DEV_ID" "$CA_PPRS" "$EC_API_OA2")
       #loggerUp "$LOG_URL" "$sk" | tee -a "$INST_LOG"
-      loggerUp "$EC_SEED_HOST" "$sk"
+      loggerUp "$EC_SEED_HOST" "$sk" | tee -a "$INST_LOG"
     } && {
       echo logger terminated on ["$x"]
       break
